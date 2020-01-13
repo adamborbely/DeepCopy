c96fae4d990ee07996a600ece2516befd6aba1da
adamborbely <55229806+adamborbely@users.noreply.github.com>
2020-01-13 14:54:41 +0100

    Deep copy vs shallow copy

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recepie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var recA = new Recipe(1, "CupCake",
+                new List<string>() { "rice", "sugar", "oil", "milk" });
+            var recB = new Recipe(2, "Hot tube",
+                  new List<string>() { "eggs", "sugar", "flour", "water" });
+            var recC = new Recipe(3, "Christmas cookies",
+                new List<string>() { "mango", "cinamon", "flour", "vodka" });
+            var recD = new Recipe(4, "Mom's favourite",
+                new List<string>() { "milk", "ice", "coffe", "vodka" });
+            var recE = new Recipe(5, "Niko",
+                new List<string>() { "horse meat", "bannana", "puding", });
+
+            Console.WriteLine(recA);
+            Console.WriteLine(recB);
+            Console.WriteLine(recC);
+            Console.WriteLine(recD);
+            Console.WriteLine(recE);
+
+            var recAC = (Recipe)recA.Clone();
+            var recADeepC = new Recipe(recA);
+            var recBC = (Recipe)recB.Clone();
+            var recBDeepC = new Recipe(recB);
+            var recDC = (Recipe)recD.Clone();
+            var recDDeepC = new Recipe(recD);
+
+
+            recAC.Ingredients.Add("bear");
+            recAC.ID++;
+            recBC.Name = "Cold tuna";
+            recBC.Ingredients.Remove("eggs");
+            recADeepC.Ingredients.Add("sor");
+            recBDeepC.Name = "Mateuska";
+            recDC.ID = 67;
+            recDC.Ingredients.Clear();
+
+            Console.WriteLine("Deep copy A after mod " + recADeepC);
+            Console.WriteLine("A after mod " + recA);
+            Console.WriteLine("Clone A: " + recAC);
+            Console.WriteLine("Deep copy B after mod " + recBDeepC);
+            Console.WriteLine("After mod" + recB);
+            Console.WriteLine("Clone B: " + recBC);
+            Console.WriteLine("Deep copy D after mod " + recDDeepC);
+            Console.WriteLine("After mod" + recD);
+            Console.WriteLine("Clone D: " + recDC);
+
+        }
+    }
+}
---
+++ Recipe.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recepie
+{
+    class Recipe : ICloneable
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<string> Ingredients = new List<string>();
+
+        public Recipe(int id, string name, List<string> ingredients)
+        {
+
+            this.ID = id;
+            this.Name = name;
+            this.Ingredients = ingredients;
+        }
+        public Recipe(Recipe recipe)
+        {
+            this.ID = recipe.ID;
+            this.Name = recipe.Name;
+            this.Ingredients = new List<string>(recipe.Ingredients);
+        }
+
+        public object Clone()
+        {
+            Recipe result = (Recipe)this.MemberwiseClone();
+            //result.Ingredients = new List<string>(Ingredients);
+            return result;
+        }
+
+
+        public override string ToString()
+        {
+
+            if (Ingredients != null)
+            {
+                string result = $"The ingredients of the {this.Name}: ";
+                for (int i = 0; i < this.Ingredients.Count; i++)
+                {
+
+                    {
+                        result += (Ingredients[i] + " ");
+
+                    }
+                }
+                return result;
+            }
+            else return "Give some ingredients first";
+
+        }
+
+
+    }
+}
